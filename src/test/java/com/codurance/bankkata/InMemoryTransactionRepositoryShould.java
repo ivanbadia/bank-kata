a9b2406a91875a3816f7bc7066844a50f25d1841
ivanbadia <ivanbadia@gmail.com>
2017-09-07 09:16:58 +0100

    change repository test in order to test two inserts

--- src/test/java/com/codurance/bankkata/InMemoryTransactionRepositoryShould.java
+++ src/test/java/com/codurance/bankkata/InMemoryTransactionRepositoryShould.java
@@ -4,18 +4,22 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class InMemoryTransactionRepositoryShould {
 
-    private static final Transaction TRANSACTION = new Transaction(LocalDate.of(2016, 9, 6), 1000);
     private final TransactionRepository transactionRepository = new InMemoryTransactionRepository();
 
     @Test
     public void store_transaction(){
-        transactionRepository.add(TRANSACTION);
+        Transaction firstTransaction = new Transaction(LocalDate.of(2016, 9, 6), 1000);
+        transactionRepository.add(firstTransaction);
+        Transaction secondTransaction = new Transaction(LocalDate.of(2016, 9, 7), -1000);
+        transactionRepository.add(secondTransaction);
 
-        assertThat(transactionRepository.all()).contains(TRANSACTION);
+        assertThat(transactionRepository.all())
+                .isEqualTo(asList(firstTransaction, secondTransaction));
     }
 
 }
